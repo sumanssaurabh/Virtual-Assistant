420beb3527017017de971e345ca4160559fc94bb
Suman Saurabh <30201452+sumanssaurabh@users.noreply.github.com>
2018-05-15 16:28:42 +0530

    Define the function

--- beta.py
+++ beta.py
@@ -4,24 +4,35 @@ from time import ctime
 import time
 import os
 
-speak = wincl.Dispatch("SAPI.SpVoice")
-text="Hello World"
-speak.Speak(text)
+
+def speak(text):
+    print(text)
+    s= wincl.Dispatch("SAPI.SpVoice")
+    s.Speak(text)
+    
  
  # Record Audio
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
- 
-# Speech recognition using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+def recordAudio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        speak("Say something")
+        audio = r.listen(source)
+    
+    data=r.recognize_google(audio)
+    # Speech recognition using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("You said: " +data)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return data
+  
+speak("Good Morning")
+data=recordAudio()
 
+if "what time is it" in data:
+    speak(ctime())
